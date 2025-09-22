9a4c3418621750f9feb1b262d18bf06140684f33
Naveen N <78733685+naveennagarajan34@users.noreply.github.com>
2025-09-22 18:45:08 +0530

    Maps collections program added

---
+++ src/collections/oldestPersonInMap.java
@@ -0,0 +1,41 @@
+package collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class oldestPersonInMap {
+
+	public static void main(String[] args) {
+		Map<String, Integer> employees = new HashMap<>();
+		employees.put("Amit", 34);
+		employees.put("Sumit", 26);
+		employees.put("Naveen", 24);
+		employees.put("Ravi", 41);
+		employees.put("Priya", 29);
+		employees.put("Sneha", 32);
+		employees.put("Karan", 38);
+		employees.put("Meena", 27);
+		employees.put("Vikram", 36);
+		employees.put("Anita", 30);
+
+		int older = 0;
+		String olderPerson = "";
+		if(employees.containsKey("Naveen")) {
+			System.out.println("Yes we have the employee Naveen in our list");
+		}
+		if(employees.containsValue(26)) {
+			System.out.println("Yes we have a employee with age 26");
+		}
+		if(employees.containsKey("Meena")) {
+			employees.remove("Meena");
+			System.out.println("Employee Meena removed from the list");
+		}
+		for (Map.Entry<String, Integer> entry : employees.entrySet()) {
+			if (entry.getValue() > older) {
+				older = entry.getValue();
+				olderPerson = entry.getKey();
+			}
+		}
+		System.out.println("Oldest Employee in the Employee list is --> " + olderPerson);
+	}
+}
---
+++ src/strings/PalinedromeTypeString.java
@@ -0,0 +1,13 @@
+package strings;
+
+public class PalinedromeTypeString {
+
+	public static void main(String[] args) {
+		String str = "Data";
+		int flag = 1;
+		int len = str.length();
+		for (int i = 0; i < len; i++) {
+			System.out.print(str.charAt(i));
+		}
+	}
+}
