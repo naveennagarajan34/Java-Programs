956b59b2bccf0469a19f9e87d0d859f0a2b2880b
Naveen <naveennagarajan34@gmail.com>
2025-12-22 19:33:48 +0530

    code push strings

--- src/strings/PalinedromeTypeString.java
+++ src/strings/PalinedromeTypeString.java
@@ -3,11 +3,24 @@ package strings;
 public class PalinedromeTypeString {
 
 	public static void main(String[] args) {
-		String str = "Data";
+		String str = "radar";
 //		int flag = 1;
 		int len = str.length();
-		for (int i = 0; i < len; i++) {
-			System.out.print(str.charAt(i));
+
+		String reverse = new StringBuilder(str).reverse().toString();
+		StringBuilder sb = new StringBuilder();
+		System.out.println(reverse);
+
+		String temp = "";
+		for (int i = str.length() - 1; i >= 0; i--) {
+//			System.out.print(str.charAt(i));
+			temp = temp + str.charAt(i);
+			sb.append(str.charAt(i));
 		}
+
+		if (str.equalsIgnoreCase(sb.toString()))
+			System.out.println("Palinedrome...");
+		else
+			System.out.println("Not a Palinedrome");
 	}
 }
