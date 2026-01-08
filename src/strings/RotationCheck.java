0808091dc28dd2b66bd470a1b5ccde6bf29f79ac
Naveen <naveennagarajan34@gmail.com>
2026-01-08 13:54:13 +0530

    Rotate characters

--- src/strings/RotationCheck.java
+++ src/strings/RotationCheck.java
@@ -3,13 +3,21 @@ package strings;
 public class RotationCheck {
 
 	public static void main(String[] args) {
-		String str1 = "ABCDE";
+		String str1 = "abcde";
 		String str2 = "CDEAB";
 		String temp = str1 + str1;
 		if (temp.contains(str2))
 			System.out.println("Rotation String");
 		else
 			System.out.println("Not a Rotation String");
+
+		// rotate left cdeab
+		int n = str1.length();
+		int k = 2 % n;
+		System.out.println(str1.substring(k) + str1.substring(0, k));
+
+		// rotate right deabc
+		System.out.println(str1.substring(n - k) + str1.substring(0, n - k));
 
 	}
 
